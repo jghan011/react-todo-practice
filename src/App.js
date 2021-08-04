37d5030e1c045a08cdec6b1d7380843dad8293b5
Jason <jghan011@gmail.com>
2021-08-04 16:44:08 -0500

    Changes done to the practice app for todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [shoppingItem,setShoppingItem] = React.useState([]);
+const [itemName,setItemName] = React.useState("");
+const [editItem,setEditItem] = React.useState(null);
+const [newItemText,setNewItemText] = React.useState('')
+
+
+function handleSubmit(e) {
+  e.preventDefault();
+  var date = new Date();
+  var string = date.toLocaleTimeString();
+  const newItems ={
+    id:string,
+    name:itemName,
+    completed:false,
+
+  }
+  setShoppingItem([...shoppingItem].concat(newItems));
+  setItemName('')
+}
+
+ function deleteItem(id1) {
+
+  const updatedFromDelete = [...shoppingItem].filter((cartItem)=>{
+    return cartItem.id !== id1
+
+  });
+  setShoppingItem(updatedFromDelete);
+}
+
+function submitItem(id1) {
+  const updatedFromSubmit = [...shoppingItem].map((cartItem)=>{
+    if(cartItem.id === id1) {
+      cartItem.name = newItemText
+    }
+    return cartItem
+  })
+  setEditItem(updatedFromSubmit)
+
+
+}
+
+
+return (
+  <div>
+
+    <form onSubmit={handleSubmit}>
+
+    <input type="text" onChange={((e)=> setItemName(e.target.value))} value={itemName}/>
+    <button type="submit">add button</button>
+
+    </form>
+ 
+     {shoppingItem.map((todo)=>
+      <div key={todo.id}>
+        {editItem === todo.id ? <input
+          type="text"
+          onChange={(e)=>setNewItemText(e.target.value)}
+          value={newItemText}
+        
+        />:<div>{todo.name}</div>}
+       
+         <button onClick={()=> deleteItem(todo.id)}>delete</button> 
+
+        <button onClick={()=> setEditItem(todo.id)}>Edit</button>
+        <button onClick={()=> submitItem(todo.id)}>submit</button>
+        </div>
+
+    )} 
+
+  </div>
+ 
+  
+
+)
+
+
+
 }
 
 export default App;
