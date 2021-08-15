7a1ea568afd295b8849af59a6badf9ffaff24350
Jason <jghan011@gmail.com>
2021-08-15 17:51:37 -0500

    changes made to todo.

--- src/App.js
+++ src/App.js
@@ -1,85 +1,78 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
 
 function App() {
-const [shoppingItem,setShoppingItem] = React.useState([]);
-const [itemName,setItemName] = React.useState("");
-const [editItem,setEditItem] = React.useState(null);
-const [newItemText,setNewItemText] = React.useState('')
+  const [shoppingItems, setShoppingItems] = React.useState([]);
+  const [itemName, setItemName] = React.useState("");
+  const [editItem, setEditItem] = React.useState(null);
+  const [editText, setEditText] = React.useState("");
 
+  //will run once when component is mounted right when page is loaded the code will be loaded basically. if you
+  function handleSubmit(e) {
+    e.preventDefault();
 
-function handleSubmit(e) {
-  e.preventDefault();
-  var date = new Date();
-  var string = date.toLocaleTimeString();
-  const newItems ={
-    id:string,
-    name:itemName,
-    completed:false,
+    let dates = new Date();
+    let times = dates.toLocaleTimeString();
 
+    const newItems = {
+      id: times,
+      name: itemName,
+      completed: false,
+    };
+
+    setShoppingItems([...shoppingItems].concat(newItems));
+    setItemName("");
   }
-  setShoppingItem([...shoppingItem].concat(newItems));
-  setItemName('')
-}
 
- function deleteItem(id1) {
+  function handleDelete(idOne) {
+    const updateAfterDelete = [...shoppingItems].filter((cartItem) => {
+      return cartItem.id !== idOne;
+    });
+    setShoppingItems(updateAfterDelete);
+    setItemName("");
+  }
 
-  const updatedFromDelete = [...shoppingItem].filter((cartItem)=>{
-    return cartItem.id !== id1
-
-  });
-  setShoppingItem(updatedFromDelete);
-}
-
-function submitItem(id1) {
-  const updatedFromSubmit = [...shoppingItem].map((cartItem)=>{
-    if(cartItem.id === id1) {
-      cartItem.name = newItemText
-    }
-    return cartItem
-  })
-  setEditItem(updatedFromSubmit)
-
-
-}
-
-
-return (
-  <div>
-
-    <form onSubmit={handleSubmit}>
-
-    <input type="text" onChange={((e)=> setItemName(e.target.value))} value={itemName}/>
-    <button type="submit">add button</button>
-
-    </form>
- 
-     {shoppingItem.map((todo)=>
-      <div key={todo.id}>
-        {editItem === todo.id ? <input
+  function submitItem(idTwo) {
+    const submitItems = [...shoppingItems].map((cartItemTwo) => {
+      if (cartItemTwo.id === idTwo) {
+        cartItemTwo.name = editText;
+      }
+      return cartItemTwo;
+    });
+    setShoppingItems(submitItems);
+    setEditItem(null);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
           type="text"
-          onChange={(e)=>setNewItemText(e.target.value)}
-          value={newItemText}
-        
-        />:<div>{todo.name}</div>}
-       
-         <button onClick={()=> deleteItem(todo.id)}>delete</button> 
+          onChange={(e) => setItemName(e.target.value)}
+          value={itemName}
+        />
+        <button>Add Something</button>
+      </form>
+      {shoppingItems.map((todo) => {
+        return (
+          <div key={todo.name}>
+            {editItem === todo.id ? (
+              <input
+                type="text"
+                onChange={(e) => setEditText(e.target.value)}
+                value={editText}
+              />
+            ) : (
+              <div>{todo.name}</div>
+            )}
 
-        <button onClick={()=> setEditItem(todo.id)}>Edit</button>
-        <button onClick={()=> submitItem(todo.id)}>submit</button>
-        </div>
-
-    )} 
-
-  </div>
- 
-  
-
-)
-
-
-
+            <button onClick={() => handleDelete(todo.id)}>delete</button>
+            <button onClick={() => setEditItem(todo.id)}>Edit</button>
+            <button onClick={() => submitItem(todo.id)}>Submit</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
